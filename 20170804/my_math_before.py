230305cd8c0be7e80c7c541937f3097710c1502f
sho <sho@sho-no-MacBook-Air.local>
2017-08-04 21:01:27 +0900

    commit 20170804

---
+++ 20170804/my_math_before.py
@@ -0,0 +1,12 @@
+# python 3.5.2
+
+def get_fibonacci_by_index(index):
+    if index in [1, 2]:
+        return 1
+    fibs =  [1, 1]
+
+    for i in range(3, index + 1):
+        c = fibs[i - 2] + fibs[i-3]
+        fibs.append(c)
+
+    return fibs[-1]
